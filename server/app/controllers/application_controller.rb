ef49d10e0236db71c43cc782b2619280a43444f7
InJung Chung <mu29gl@gmail.com>
2015-11-09 18:39:47 +0900

    CORS issue

--- server/app/controllers/application_controller.rb
+++ server/app/controllers/application_controller.rb
@@ -1,5 +1,12 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
+  before_filter :set_headers
   protect_from_forgery with: :null_session
+
+
+  private
+  def set_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'GET, POST, PATCH, PUT, DELETE, OPTIONS, HEAD'
+    headers['Access-Control-Allow-Headers'] = '*'
+  end
 end
